1e60e846118e60ca2a92ae8d9ea50ff275f11d6e
hasithaPerera2002 <hasithaperera2017@gmail.com>
2025-07-03 00:31:46 +0530

    fix: update wallet connection rendering to show when not connected

--- app/components/BingoGame.tsx
+++ app/components/BingoGame.tsx
@@ -268,7 +268,7 @@ export function BingoGame() {
           </div>
 
           {/* Wallet Connection */}
-          { isConnected && (
+          { !isConnected && (
             <div className="mb-8 z-10 relative">
               <WalletConnection />
             </div>)
